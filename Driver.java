92e5a999baf18ae762f138d3aaf0eab77874b761
hahaws666 <109090362+hahaws666@users.noreply.github.com>
2023-09-28 16:34:32 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,16 +1,44 @@
+import java.io.File;
+import java.io.IOException;
+
 public class Driver {
-public static void main(String [] args) {
-    Polynomial p = new Polynomial();
-    System.out.println(p.evaluate(3));
-    double [] c1 = {6,0,0,5};
-    Polynomial p1 = new Polynomial(c1);
-    double [] c2 = {0,-2,0,0,-9};
-    Polynomial p2 = new Polynomial(c2);
-    Polynomial s = p1.add(p2);
-    System.out.println("s(0.1) = " + s.evaluate(0.1));
-    if(s.hasRoot(1))
-    System.out.println("1 is a root of s");
-    else
-    System.out.println("1 is not a root of s");
-}
+    public static void main(String[] args) throws IOException {
+        double[] coeffs1 = {-1,1};
+        int[] exp1 = {0,1};
+
+        double[] coeffs2 = {1,1};
+        int[] exp2 = {1,0};
+
+        Polynomial poly1 = new Polynomial(coeffs1, exp1);
+        Polynomial poly2 = new Polynomial(coeffs2, exp2);
+
+        Polynomial sum = poly1.add(poly2);
+        Polynomial product = poly1.multiply(poly2);
+        System.out.println("Poly1: " + poly1);
+        System.out.println("Poly2: " + poly2);
+        System.out.println("Sum: " + sum);
+        System.out.println("Product: " + product);
+        System.out.println("Evaluate Poly1 at x=-1: " + poly1.evaluate(-0.99));
+        System.out.println("Evaluate Poly2 at x=2: " + poly2.evaluate(2));
+
+        double rootValue = 1.5;
+        System.out.println("Poly1 has root at x=" + rootValue + ": " + poly1.hasRoot(rootValue));
+
+
+        
+
+
+        double[] coeffs = {1.0, -2.0, 3.0,99};
+        int[] exps = {0, 1, 2,80};
+    
+        Polynomial polynomial = new Polynomial(coeffs, exps);
+
+        polynomial.saveToFile("polynomial1.txt");
+        System.out.println("saved to polynomial1.txt");
+
+        File inputFile = new File("polynomial.txt");
+        Polynomial polyFromFile = new Polynomial(inputFile);
+		System.out.println("Polynomial from file: " + polyFromFile);
+
+    }
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,52 +1,164 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Polynomial {
     double[] coff;
-    public Polynomial() {
-        coff = new double[]{0};
+    int[] degree;
+    public Polynomial(){
+        coff=new double[] {0};
+        degree=new int[]{0};
     }
 
-    public Polynomial(double[] coefficients) {
-        this.coff = coefficients.clone();
+    public Polynomial(double[] cof, int[] deg){
+        this.coff=cof.clone();
+        this.degree=deg.clone();
     }
+    public Polynomial add(Polynomial p1){
+        Polynomial ress=new Polynomial(this.coff.clone(),this.degree.clone());
+        List<Double> cof= new ArrayList<>();
+        List<Integer> exp = new ArrayList<>();
+        int a=0;
+        int b=0;
+        while (b<p1.degree.length) {
+            int oo=0;
+            for (int i = 0; i < ress.degree.length; i++) {
 
-    public Polynomial add(Polynomial p1) {
-        int smm;
-    	double largest [];
-    	
-    	
-    	if((p1.coff).length < coff.length) {
-    		smm = (p1.coff).length;
-    		largest = coff;
-    	} else {
-    		smm = coff.length;
-    		largest = (p1.coff);
-    	}
+                if (p1.degree[b]==ress.degree[i]) {
+                    oo=1;
+                    ress.coff[i]+=p1.coff[b];
+                    if (ress.coff[i]!=0) {
+                        cof.add(ress.coff[i]);
+                        exp.add(ress.degree[i]);
+                    }
+                }
+            }
+            if(oo==0){
+            cof.add(p1.coff[b]);
+            exp.add(p1.degree[b]);
+            }
+            b++;
+        }
+        while (a<ress.degree.length) {
+            int k=0;
+            for (int i = 0; i < exp.size(); i++) {
 
-    	Polynomial newCoefficients = new Polynomial();
-    	newCoefficients.coff = new double[largest.length];
-    	    	
-    	for (int i = 0; i < largest.length; i++) {
-    		if(i >= smm) {
-    			newCoefficients.coff[i] = largest[i];
-    		} else {
-    			newCoefficients.coff[i] = coff[i] + (p1.coff)[i];
-    		}
-    	}
-    	return newCoefficients;
+                if (ress.degree[a]==exp.get(i)) {
+                    k=1;
+                }
+            }
+            if (k==0) {
+                    exp.add(ress.degree[a]);
+                    cof.add(ress.coff[a]);
+                }
+            a++;
+            
+        }
+        double[] resultCoefficients=cof.stream().mapToDouble(Double::doubleValue).toArray();
+        int[] resultExponents = exp.stream().mapToInt(Integer::intValue).toArray();
+        return new Polynomial(resultCoefficients, resultExponents);
+
     }
 
     public double evaluate(double x) {
         double result = 0;
         for (int i = 0; i < coff.length; i++) {
-            result += coff[i] * Math.pow(x, i);
+            result += coff[i] * Math.pow(x, degree[i]);
         }
         return result;
     }
+    
 
     public boolean hasRoot(double x) {
         return evaluate(x) == 0;
     }
 
+    public Polynomial multiply(Polynomial p1){
+        Polynomial res=new Polynomial();
+        for (int i = 0; i < this.coff.length; i++) {
+            for (int j = 0; j < p1.coff.length; j++) {
+                double[] cof=new double[1];
+
+                double ppp=(this.coff[i])*(p1.coff[j]);
+                cof[0]=ppp;
+                int[] deg=new int[1]; 
+                deg[0]=this.degree[i]+p1.degree[j];
+                Polynomial neww=new Polynomial(cof, deg);
+                res=res.add(neww);
+            }
+        }
+        return res;
+    }
 
 
+    public Polynomial(File file) throws IOException {
+        try (Scanner scanner = new Scanner(file)) {
+            if (scanner.hasNext()) {
+                String polynomialString = scanner.nextLine();
+                parsePolynomial(polynomialString);
+            } else {
+                coff = new double[]{0};
+                degree = new int[]{0};
+            }
+        }
+    }
 
+    private void parsePolynomial(String polynomialString) {
+        String[] termStrings = polynomialString.split("(?=[+-])");
+    
+        List<Double> coefficientList = new ArrayList<>();
+        List<Integer> exponentList = new ArrayList<>();
+    
+        for (String term : termStrings) {
+            String[] parts = term.split("x");
+    
+            if (parts.length == 1) {
+
+                    coefficientList.add(Double.parseDouble(parts[0]));
+                    exponentList.add(0);
+            } else {
+                coefficientList.add(Double.parseDouble(parts[0]));
+                exponentList.add(Integer.parseInt(parts[1]));
+            }
+        }
+        coff = coefficientList.stream().mapToDouble(Double::doubleValue).toArray();
+        degree = exponentList.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        boolean f = true;
+    
+        for (int i = 0; i < coff.length; i++) {
+            if (coff[i] != 0) {
+                if (!f) {
+                    if (coff[i]>0) {
+                        builder.append("+");
+                    } else {
+                        builder.append("-");
+                    }
+                } else {
+                    f = false;
+                }
+                double absCoefficient = Math.abs(coff[i]);
+                builder.append(absCoefficient);
+                if (degree[i] != 0) {
+                    builder.append("x");
+                        builder.append(degree[i]);
+                }
+            }
+        }
+        return builder.toString().replace(" ", "");
+    }
+
+    public void saveToFile(String fileName) throws IOException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
+            writer.write(this.toString());
+            writer.close();
+        }
+    }
 }
