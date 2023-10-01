4ef7fb9659112106fb7783767c1a5f795f0da4c9
hahaws666 <109090362+hahaws666@users.noreply.github.com>
2023-10-01 00:08:00 -0400

    Modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -3,11 +3,11 @@ import java.io.IOException;
 
 public class Driver {
     public static void main(String[] args) throws IOException {
-        double[] coeffs1 = {-1,1};
-        int[] exp1 = {0,1};
+        double[] coeffs1 = {1,2,3};
+        int[] exp1 = {1,2,4};
 
-        double[] coeffs2 = {1,1};
-        int[] exp2 = {1,0};
+        double[] coeffs2 = {-1,5,6,7,9};
+        int[] exp2 = {2,3,5,6,7};
 
         Polynomial poly1 = new Polynomial(coeffs1, exp1);
         Polynomial poly2 = new Polynomial(coeffs2, exp2);
@@ -18,8 +18,8 @@ public class Driver {
         System.out.println("Poly2: " + poly2);
         System.out.println("Sum: " + sum);
         System.out.println("Product: " + product);
-        System.out.println("Evaluate Poly1 at x=-1: " + poly1.evaluate(-0.99));
-        System.out.println("Evaluate Poly2 at x=2: " + poly2.evaluate(2));
+        System.out.println("Poly1 at x=-1:" + poly1.evaluate(-0.99));
+        System.out.println("Poly2 at x=2: " + poly2.evaluate(2));
 
         double rootValue = 1.5;
         System.out.println("Poly1 has root at x=" + rootValue + ": " + poly1.hasRoot(rootValue));
@@ -28,17 +28,17 @@ public class Driver {
         
 
 
-        double[] coeffs = {1.0, -2.0, 3.0,99};
+        double[] coeffs = {-11.0, -2.0, 3.0,99};
         int[] exps = {0, 1, 2,80};
     
         Polynomial polynomial = new Polynomial(coeffs, exps);
 
         polynomial.saveToFile("polynomial1.txt");
-        System.out.println("saved to polynomial1.txt");
+        System.out.println("saved");
 
         File inputFile = new File("polynomial1.txt");
         Polynomial polyFromFile = new Polynomial(inputFile);
-		System.out.println("Polynomial from file: " + polyFromFile);
+		System.out.println("Polynomial from file: " + polyFromFile.toString());
 
     }
 }
